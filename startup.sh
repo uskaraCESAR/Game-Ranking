1949c29165240d9e420f7caf5b0717819ecefd65
Arthur Reis <afsr@cesar.school>
2026-04-16 18:02:35 -0300

    Set password for user 'gabeitch' in startup script
    Added command to set password for user 'gabeitch'.

--- startup.sh
+++ startup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Running Django migrations..."
 python manage.py migrate --noinput
+echo "Setting gabeitch password..."
+python manage.py shell -c "from django.contrib.auth import get_user_model; U=get_user_model(); u=U.objects.get(username='gabeitch'); u.set_password('GameRanking@2026!'); u.save(); print('PASSWORD SET OK')"
 echo "Starting gunicorn..."
 gunicorn Game_Ranking.wsgi:application --bind 0.0.0.0:8000
